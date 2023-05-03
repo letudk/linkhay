3b0309627fef3cb354633df0d1c3016d82b688eb
letudk <tule.itc@gmail.com>
2023-05-03 20:34:31 +0700

    g

--- content.php
+++ content.php
@@ -1,14 +1,11 @@
-<?php 
-global $fox_options;
-?>
 <article class="new-card">
     <div class = "project-item">
         <div class = "image-project">
-            <img class = "img-project" src = "https://ph-files.imgix.net/95497a20-f3cd-4687-8e3c-87c748bfd0a1.png"></img>
+            <img class = "img-project" src="<?php echo get_the_post_thumbnail_url (get_the_ID()); ?>"></img>
         </div>
         <div class = "content-project">
-            <h1>monday.com</h1>
-            <p>Build your ideal workflow with 200+ customizable templates</p>
+            <h1><?php echo get_the_title(); ?></h1>
+            <p><?php echo wp_trim_words( get_the_excerpt() , 10 ) ?></p>
         </div>
         <div class = "upvote">
             <i class="fas fa-caret-up"></i>
